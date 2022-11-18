dbdbc2498bc971afb62a6d33800141ff54f7d0b2
JUNIOR <junioralberto1421@gmail.com>
2022-11-18 10:17:57 -0600

    Actualizacion

---
+++ Semana_16/Controllers/AyudaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana_16.Controllers
+{
+    public class AyudaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ayuda(string name, int numTimes = 1)
+        {
+
+            ViewData["name"] = "Hola " + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+    }
+}
